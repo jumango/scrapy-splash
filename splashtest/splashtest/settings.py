9e4de3347cd26ebdac1fd9766985aca1533879bd
Paul Tremberth <paul.tremberth@gmail.com>
2014-10-06 20:14:08 +0200

    [WIP] Sample project embedding a splash middleware

---
+++ splashtest/splashtest/settings.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for splashmw project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'splashtest'
+
+SPIDER_MODULES = ['splashtest.spiders']
+NEWSPIDER_MODULE = 'splashtest.spiders'
+
+DOWNLOADER_MIDDLEWARES = {
+    'splashtest.middleware.splash.SplashMiddleware': 725,
+}
+
+SPLASH_ENABLED = True
+SPLASH_URL = 'http://localhost:8050/render.html'
+SPLASH_WAIT = 2
+SPLASH_IMAGES = False
+#SPLASH_URL_PASS = (r'example\.com',)
+#SPLASH_URL_BLOCK = (r'badexample\.com',)
+
+CONCURRENT_REQUESTS_PER_DOMAIN = 5
+CONCURRENT_REQUESTS = 5
+RETRY_ENABLED = False
---
+++ splashtest/splashtest/spiders/example.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+import cStringIO as StringIO
+import scrapy
+
+
+class ExampleSpider(scrapy.Spider):
+    name = "example"
+
+    def start_requests(self):
+        yield scrapy.Request(self.urls, callback=self.parse_urlfile,
+                             meta={"use_splash": False})
+
+    def parse_urlfile(self, response):
+        limit = int(getattr(self, 'limit', '0'))
+        for cnt, line in enumerate(StringIO.StringIO(response.body).readlines(), start=1):
+            yield scrapy.Request(line.strip())
+            if limit and cnt >= limit:
+                break
+
+    def parse(self, response):
+        self.log("response.url:         %r" % response.url)
+        self.log("response.request.url: %r" % response.request.url)
+        splashed = response.meta.get("splashed_url")
+        if splashed:
+            self.log("Splashed request:     %r" % splashed)
